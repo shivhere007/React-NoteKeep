bbe12592a3dfc1d20a28b18739217a5453f01819
Shivam Prasad Singh <singhshivam7766881456@gmail.com>
2024-11-27 21:13:43 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>NoteKeep</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./../public/styles.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
